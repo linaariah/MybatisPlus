7b6be0b92eb85698a72a0a911b4b27379ec854e5
Naariah <linaariah@126.com>
2022-06-01 16:37:51 +0800

    first windows commit

--- mybatisplus_02_dql/src/test/java/com/Naariah/Mybatisplus02DqlApplicationTests.java
+++ mybatisplus_02_dql/src/test/java/com/Naariah/Mybatisplus02DqlApplicationTests.java
@@ -1,13 +1,24 @@
 package com.Naariah;
 
+import com.Naariah.dao.UserDao;
+import com.Naariah.domain.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class Mybatisplus02DqlApplicationTests {
+    @Autowired
+    private UserDao userDao;
 
     @Test
-    void contextLoads() {
+    void testGetAll(){
+        List<User> userList = userDao.selectList(null);
+        System.out.println(userList);
     }
+
+
 
 }
